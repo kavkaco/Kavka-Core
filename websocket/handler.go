ce3dee8e0f29508726b4a364fe6ee89c050cf9b0
Taha Dostifam <mr.tahadostifam@gmail.com>
2022-06-22 20:37:04 +0430

    websocket init almost finished

--- websocket/handler.go
+++ websocket/handler.go
@@ -11,6 +11,22 @@ type SSendMessage struct {
 	Text string `json:"text"`
 }
 
+var ws = websocket.New(func(c *websocket.Conn) {
+	fmt.Println("user is upgraded")
+	fmt.Println(c.Params("id"))
+	fmt.Println(c.Locals("accepted"))
+
+	for {
+		msgType, msg, msgErr := c.ReadMessage()
+		if msgErr != nil {
+			logs.ErrorLogger.Println("Error in reading socket message.")
+			break
+		}
+
+		WebSocketMessageHandler(string(msg), msgType, c)
+	}
+})
+
 func WebSocketMessageHandler(msg string, msgType int, c *websocket.Conn) {
 	var s *SSendMessage = &SSendMessage{}
 	err := c.ReadJSON(s)
--- websocket/init.go
+++ websocket/init.go
@@ -1,22 +1,18 @@
 package websocket
 
 import (
-	"Tahagram/logs"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 )
 
 func InitWebSocket(app *fiber.App) {
-	app.Use("/ws", websocket.New(func(c *websocket.Conn) {
-		for {
-			msgType, msg, msgErr := c.ReadMessage()
-			if msgErr != nil {
-				logs.ErrorLogger.Println("Error in reading socket message.")
-				break
-			}
-
-			WebSocketMessageHandler(string(msg), msgType, c)
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("accepted", true)
+			return c.Next()
 		}
-	}))
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/ws", ws)
 }
