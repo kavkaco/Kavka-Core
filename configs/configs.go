fc203eca60be2df101ec2e36b8b78d552b94fc40
Taha Dostifam <mr.tahadostifam@gmail.com>
2022-08-29 12:01:30 +0430

    [init]: config parser

--- configs/configs.go
+++ configs/configs.go
@@ -1,1 +1,66 @@
 package configs
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type IConfigs struct {
+	App struct {
+		Name string `yaml:"NAME"`
+
+		HTTP struct {
+			Host    string `yaml:"HOST"`
+			Port    int    `yaml:"PORT"`
+			Address string `yaml:"ADDRESS"`
+		} `yaml:"HTTP"`
+
+		Fiber struct {
+			ServerHeader string `yaml:"SERVER_HEADER"`
+			Prefork      bool   `yaml:"PREFORK"`
+			CORS         struct {
+				AllowOrigins     string `yaml:"ALLOW_ORIGINS"`
+				AllowCredentials bool   `yaml:"ALLOW_CREDENTIALS"`
+			} `yaml:"CORS"`
+		} `yaml:"FIBER"`
+	} `yaml:"APP"`
+
+	Redis struct {
+		Host     string `yaml:"HOST"`
+		Username string `yaml:"USERNAME"`
+		Password string `yaml:"PASSWORD"`
+		Port     int    `yaml:"PORT"`
+	}
+
+	Mongo struct {
+		Host     string `yaml:"HOST"`
+		Username string `yaml:"USERNAME"`
+		Password string `yaml:"PASSWORD"`
+		Port     int    `yaml:"PORT"`
+		DBName   string `yaml:"DB_NAME"`
+	}
+
+	SMTP struct {
+		Host     string `yaml:"HOST"`
+		Port     int    `yaml:"PORT"`
+		Email    string `yaml:"EMAIL"`
+		Password string `yaml:"PASSWORD"`
+	}
+}
+
+func Read(fileName string) (IConfigs, error) {
+	var cfg *IConfigs
+
+	data, readErr := os.ReadFile(fileName)
+	if readErr != nil {
+		return IConfigs{}, readErr
+	}
+
+	parseErr := yaml.Unmarshal(data, &cfg)
+	if parseErr != nil {
+		return IConfigs{}, parseErr
+	}
+
+	return *cfg, nil
+}
--- go.mod
+++ go.mod
@@ -4,12 +4,17 @@ go 1.18
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gofiber/fiber/v2 v2.36.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.39.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20220826181053-bd7e27e6170d // indirect
 	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
