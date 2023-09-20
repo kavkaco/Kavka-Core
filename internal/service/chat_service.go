7b16a7ce6a688a0c87ed524dd59a9866923b5d76
-Taha Dostifam‍- <mr.tahadostifam@gmail.com>
2023-09-20 14:51:41 +0330

    fix: package EOF errors and etc

--- internal/service/chat_service.go
+++ internal/service/chat_service.go
@@ -26,9 +26,7 @@ func (s *chatService) GetChat(staticID primitive.ObjectID) (*chat.Chat, error) {
 	return foundChat, nil
 }
 
-func (s *chatService) CreateDirect(userStaticID primitive.ObjectID,
-	targetStaticID primitive.ObjectID,
-) (*chat.Chat, error) {
+func (s *chatService) CreateDirect(userStaticID primitive.ObjectID, targetStaticID primitive.ObjectID) (*chat.Chat, error) {
 	sides := [2]*primitive.ObjectID{
 		&userStaticID,
 		&targetStaticID,
@@ -39,31 +37,33 @@ func (s *chatService) CreateDirect(userStaticID primitive.ObjectID,
 		return nil, chatRepository.ErrChatAlreadyExists
 	}
 
-	return s.chatRepo.Create(chat.ChatTypeDirect, &chat.DirectChatDetail{
+	newChat := chat.NewChat(chat.ChatTypeDirect, &chat.DirectChatDetail{
 		Sides: sides,
 	})
+
+	return s.chatRepo.Create(newChat)
 }
 
-func (s *chatService) CreateGroup(userStaticID primitive.ObjectID,
-	title string, username string, description string,
-) (*chat.Chat, error) {
-	return s.chatRepo.Create(chat.ChatTypeGroup, &chat.GroupChatDetail{
+func (s *chatService) CreateGroup(userStaticID primitive.ObjectID, title string, username string, description string) (*chat.Chat, error) {
+	newChat := chat.NewChat(chat.ChatTypeGroup, &chat.GroupChatDetail{
 		Title:       title,
 		Username:    username,
 		Members:     []*primitive.ObjectID{&userStaticID},
 		Admins:      []*primitive.ObjectID{&userStaticID},
 		Description: description,
 	})
+
+	return s.chatRepo.Create(newChat)
 }
 
-func (s *chatService) CreateChannel(userStaticID primitive.ObjectID,
-	title string, username string, description string,
-) (*chat.Chat, error) {
-	return s.chatRepo.Create(chat.ChatTypeGroup, &chat.GroupChatDetail{
+func (s *chatService) CreateChannel(userStaticID primitive.ObjectID, title string, username string, description string) (*chat.Chat, error) {
+	newChat := chat.NewChat(chat.ChatTypeGroup, &chat.GroupChatDetail{
 		Title:       title,
 		Username:    username,
 		Members:     []*primitive.ObjectID{&userStaticID},
 		Admins:      []*primitive.ObjectID{&userStaticID},
 		Description: description,
 	})
+
+	return s.chatRepo.Create(newChat)
 }
