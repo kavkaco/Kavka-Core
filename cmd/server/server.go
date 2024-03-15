e71cd46b61a7191e444a6443ddbdfa3322c7beda
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-03-15 19:18:16 +0330

    refactor: better naming for structs and variables

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,11 +4,8 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kavkaco/Kavka-Core/app/router"
-	"github.com/kavkaco/Kavka-Core/app/socket"
 	"github.com/kavkaco/Kavka-Core/config"
 	"github.com/kavkaco/Kavka-Core/database"
-	chatRepository "github.com/kavkaco/Kavka-Core/internal/repository/chat"
-	messageRepository "github.com/kavkaco/Kavka-Core/internal/repository/message"
 	userRepository "github.com/kavkaco/Kavka-Core/internal/repository/user"
 	"github.com/kavkaco/Kavka-Core/internal/service"
 	"github.com/kavkaco/Kavka-Core/pkg/session"
@@ -50,14 +47,14 @@ func main() {
 	userService := service.NewUserService(userRepo, session, smsService)
 	router.NewUserRouter(app.Group("/users"), userService)
 
-	chatRepo := chatRepository.NewChatRepository(mongoDB)
-	chatService := service.NewChatService(chatRepo, userRepo)
+	//chatRepo := chatRepository.NewRepository(mongoDB)
+	//chatService := service.NewChatService(chatRepo, userRepo)
 
-	messageRepo := messageRepository.NewMessageRepository(mongoDB)
-	messageRepository := service.NewMessageService(messageRepo, chatRepo)
+	//messageRepo := messageRepository.NewMessageRepository(mongoDB)
+	//messageRepository := service.NewMessageService(messageRepo, chatRepo)
 
 	// Init Socket Server
-	socket.NewSocketService(app, userService, chatService, messageRepository)
+	//socket.NewSocketService(app, userService, chatService, messageRepository)
 
 	// Everything almost done!
 	err := app.Run(configs.App.HTTP.Address)
