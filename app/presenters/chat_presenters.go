55231e2679fbe956b3e087edc14a036a2e996a3e
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-03-19 20:55:46 +0330

    fix: gathering user's chats in authentication action in user_controller completed; now user can get his chats by using this endpoint;

--- app/presenters/chat_presenters.go
+++ app/presenters/chat_presenters.go
@@ -2,6 +2,7 @@ package presenters
 
 import (
 	"github.com/kavkaco/Kavka-Core/internal/domain/chat"
+	"github.com/kavkaco/Kavka-Core/utils"
 )
 
 type ChatDto struct {
@@ -9,27 +10,27 @@ type ChatDto struct {
 	Chat  *chat.Chat `json:"chat"`
 }
 
-// FIXME
 func ChatAsJSON(obj chat.Chat) (interface{}, error) {
-	// if obj.ChatType == chat.TypeDirect {
-	// 	obj.ChatDetail = nil
-	// } else {
-	// 	// Determine the specific ChatDetail type based on chatType
-	// 	var chatDetail interface{}
-	// 	var convertErr error
+	if obj.ChatType == chat.TypeDirect {
+		obj.ChatDetail = nil
+	} else {
+		// Determine the specific ChatDetail type based on chatType
+		var chatDetail interface{}
+		var convertErr error
 
-	// 	switch obj.ChatType {
-	// 	case chat.TypeChannel:
-	// 		chatDetail, convertErr = utils.TypeConverter[chat.ChannelChatDetail](obj.ChatDetail)
-	// 	case chat.TypeGroup:
-	// 		chatDetail, convertErr = utils.TypeConverter[chat.GroupChatDetail](obj.ChatDetail)
-	// 	}
+		switch obj.ChatType {
+		case chat.TypeChannel:
+			chatDetail, convertErr = utils.TypeConverter[chat.ChannelChatDetail](obj.ChatDetail)
+		case chat.TypeGroup:
+			chatDetail, convertErr = utils.TypeConverter[chat.GroupChatDetail](obj.ChatDetail)
+		}
 
-	// 	if convertErr != nil {
-	// 		return nil, convertErr
-	// 	}
-	// 	obj.ChatDetail = chatDetail
-	// }
+		if convertErr != nil {
+			return nil, convertErr
+		}
+
+		obj.ChatDetail = chatDetail
+	}
 
 	return obj, nil
 }
