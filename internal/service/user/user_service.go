addf19b8b9bf16c20d0dfa08c97967f46deb4caf
Amir <assassimoltafet@gmail.com>
2024-06-19 13:37:30 +0330

    add:DeleteAccount method to user service

--- internal/service/user/user_service.go
+++ internal/service/user/user_service.go
@@ -5,18 +5,26 @@ import (
 
 	"github.com/kavkaco/Kavka-Core/internal/model"
 	"github.com/kavkaco/Kavka-Core/internal/repository"
+	"github.com/kavkaco/Kavka-Core/utils/hash"
 )
 
 type UserService interface {
 	UpdateProfile(ctx context.Context, userID model.UserID, name, lastName, username, biography string) error
+	DeleteAccount(ctx context.Context,userId model.UserID,password string)error
 }
 
 type UserManager struct {
 	userRepo repository.UserRepository
+	authRepo repository.AuthRepository
+	hashManager *hash.HashManager
 }
 
-func NewUserService(userRepo repository.UserRepository) UserService {
-	return &UserManager{userRepo}
+func NewUserService(userRepo repository.UserRepository,authRepo repository.AuthRepository,hashManager *hash.HashManager) UserService {
+	return &UserManager{
+		userRepo: userRepo,
+		authRepo: authRepo,
+		hashManager: hashManager,
+	}
 }
 
 func (s *UserManager) UpdateProfile(ctx context.Context, userID model.UserID, name, lastName, username, biography string) error {
@@ -46,5 +54,23 @@ func (s *UserManager) UpdateProfile(ctx context.Context, userID model.UserID, na
 		return ErrUpdateUser
 	}
 
+	return nil
+}
+func (s *UserManager) DeleteAccount(ctx context.Context, userId model.UserID, password string) error {
+	auth, err := s.authRepo.GetUserAuth(ctx, userId)
+	if err != nil {
+		return ErrNotFound
+	}
+	if !auth.EmailVerified {
+		return ErrDeleteUser
+	}
+	validPassword := s.hashManager.CheckPasswordHash(password, auth.PasswordHash)
+	if !validPassword{
+		return ErrDeleteUser
+	}
+	err = s.userRepo.DeleteById(ctx,userId)
+	if err != nil{
+		return ErrDeleteUser
+	}
 	return nil
 }
