c5ff91a5e299bd58269dc54d5c1b4b6cf87fe420
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-07-16 02:16:17 +0330

    refactor: utils/random package refactored and benchmark added

--- utils/random/random.go
+++ utils/random/random.go
@@ -3,13 +3,9 @@ package random
 import (
 	"crypto/rand"
 	"encoding/hex"
-	"fmt"
 	"math"
 	"math/big"
-	"strconv"
 )
-
-const OtpLength = 6
 
 func generateRandomNumber(length int) int {
 	min := int64(math.Pow(10, float64(length)-1))
@@ -22,23 +18,11 @@ func generateRandomNumber(length int) int {
 
 	number := int(randomNumber.Int64()) + int(min)
 
-	if len(strconv.Itoa(number)) != length {
-		number = GenerateOTP()
-	}
-
 	return number
-}
-
-func GenerateOTP() int {
-	return generateRandomNumber(OtpLength)
 }
 
 func GenerateUserID() int {
 	return generateRandomNumber(8)
-}
-
-func GenerateUsername() string {
-	return fmt.Sprintf("guest_%d", generateRandomNumber(10))
 }
 
 func GenerateRandomFileName(n int) string {
