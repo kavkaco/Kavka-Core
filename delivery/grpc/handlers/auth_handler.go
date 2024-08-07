9ca1af1b22f9fbb1be46145713efd646f1c2627f
Amir <assassinmoltafet@gmail.com>
2024-08-07 18:10:46 +0330

    refactor error handling in grpc handlers

--- delivery/grpc/handlers/auth_handler.go
+++ delivery/grpc/handlers/auth_handler.go
@@ -8,6 +8,7 @@ import (
 	grpc_model "github.com/kavkaco/Kavka-Core/delivery/grpc/model"
 	"github.com/kavkaco/Kavka-Core/internal/service/auth"
 	authv1 "github.com/kavkaco/Kavka-Core/protobuf/gen/go/protobuf/auth/v1"
+	"google.golang.org/genproto/googleapis/rpc/code"
 	"google.golang.org/protobuf/types/known/durationpb"
 )
 
@@ -22,7 +23,7 @@ func NewAuthGrpcHandler(authService auth.AuthService) AuthGrpcServer {
 func (a AuthGrpcServer) Login(ctx context.Context, req *connect.Request[authv1.LoginRequest]) (*connect.Response[authv1.LoginResponse], error) {
 	user, accessToken, refreshToken, varror := a.authService.Login(ctx, req.Msg.Email, req.Msg.Password)
 	if varror != nil {
-		return nil, grpc_helpers.GrpcVarror(varror, connect.CodeInvalidArgument)
+		return nil, grpc_helpers.GrpcVarror(varror, connect.Code(code.Code_INTERNAL))
 	}
 
 	res := connect.NewResponse(&authv1.LoginResponse{
@@ -37,7 +38,7 @@ func (a AuthGrpcServer) Login(ctx context.Context, req *connect.Request[authv1.L
 func (a AuthGrpcServer) Register(ctx context.Context, req *connect.Request[authv1.RegisterRequest]) (*connect.Response[authv1.RegisterResponse], error) {
 	_, varror := a.authService.Register(ctx, req.Msg.Name, req.Msg.LastName, req.Msg.Username, req.Msg.Email, req.Msg.Password, req.Msg.VerifyEmailRedirectUrl)
 	if varror != nil {
-		return nil, grpc_helpers.GrpcVarror(varror, connect.CodeInvalidArgument)
+		return nil, grpc_helpers.GrpcVarror(varror, connect.Code(code.Code_INVALID_ARGUMENT))
 	}
 
 	res := connect.NewResponse(&authv1.RegisterResponse{})
@@ -48,7 +49,7 @@ func (a AuthGrpcServer) Register(ctx context.Context, req *connect.Request[authv
 func (a AuthGrpcServer) Authenticate(ctx context.Context, req *connect.Request[authv1.AuthenticateRequest]) (*connect.Response[authv1.AuthenticateResponse], error) {
 	user, varror := a.authService.Authenticate(ctx, req.Msg.AccessToken)
 	if varror != nil {
-		return nil, grpc_helpers.GrpcVarror(varror, connect.CodePermissionDenied)
+		return nil, grpc_helpers.GrpcVarror(varror, connect.Code(code.Code_PERMISSION_DENIED))
 	}
 
 	res := connect.NewResponse(&authv1.AuthenticateResponse{
@@ -71,7 +72,7 @@ func (a AuthGrpcServer) ChangePassword(ctx context.Context, req *connect.Request
 func (a AuthGrpcServer) RefreshToken(ctx context.Context, req *connect.Request[authv1.RefreshTokenRequest]) (*connect.Response[authv1.RefreshTokenResponse], error) {
 	newAccessToken, varror := a.authService.RefreshToken(ctx, req.Msg.UserId, req.Msg.RefreshToken)
 	if varror != nil {
-		return nil, grpc_helpers.GrpcVarror(varror, connect.CodeUnavailable)
+		return nil, grpc_helpers.GrpcVarror(varror, connect.Code(code.Code_UNAVAILABLE))
 	}
 
 	res := connect.NewResponse(&authv1.RefreshTokenResponse{
@@ -84,7 +85,7 @@ func (a AuthGrpcServer) RefreshToken(ctx context.Context, req *connect.Request[a
 func (a AuthGrpcServer) SendResetPassword(ctx context.Context, req *connect.Request[authv1.SendResetPasswordRequest]) (*connect.Response[authv1.SendResetPasswordResponse], error) {
 	resetPasswordToken, timeout, varror := a.authService.SendResetPassword(ctx, req.Msg.Email, req.Msg.ResetPasswordRedirectUrl)
 	if varror != nil {
-		return nil, grpc_helpers.GrpcVarror(varror, connect.CodeUnavailable)
+		return nil, grpc_helpers.GrpcVarror(varror, connect.Code(code.Code_UNAVAILABLE))
 	}
 
 	timeoutProto := durationpb.New(timeout)
@@ -99,7 +100,7 @@ func (a AuthGrpcServer) SendResetPassword(ctx context.Context, req *connect.Requ
 func (a AuthGrpcServer) SubmitResetPassword(ctx context.Context, req *connect.Request[authv1.SubmitResetPasswordRequest]) (*connect.Response[authv1.SubmitResetPasswordResponse], error) {
 	varror := a.authService.SubmitResetPassword(ctx, req.Msg.ResetPasswordToken, req.Msg.NewPassword)
 	if varror != nil {
-		return nil, grpc_helpers.GrpcVarror(varror, connect.CodeUnavailable)
+		return nil, grpc_helpers.GrpcVarror(varror, connect.Code(code.Code_UNAVAILABLE))
 	}
 
 	res := connect.NewResponse(&authv1.SubmitResetPasswordResponse{})
@@ -109,7 +110,7 @@ func (a AuthGrpcServer) SubmitResetPassword(ctx context.Context, req *connect.Re
 func (a AuthGrpcServer) VerifyEmail(ctx context.Context, req *connect.Request[authv1.VerifyEmailRequest]) (*connect.Response[authv1.VerifyEmailResponse], error) {
 	varror := a.authService.VerifyEmail(ctx, req.Msg.VerifyEmailToken)
 	if varror != nil {
-		return nil, grpc_helpers.GrpcVarror(varror, connect.CodeUnavailable)
+		return nil, grpc_helpers.GrpcVarror(varror, connect.Code(code.Code_UNAVAILABLE))
 	}
 
 	res := connect.NewResponse(&authv1.VerifyEmailResponse{})
