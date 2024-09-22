e73ee7c7d7a31cf9cf37ea9d1b6421e19315c40f
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-09-22 13:39:01 +0330

    refactor docker

--- delivery/grpc/grpc_server.go
+++ delivery/grpc/grpc_server.go
@@ -1,9 +1,12 @@
 package grpc
 
 import (
+	"fmt"
 	"net/http"
 
 	"connectrpc.com/connect"
+	connectcors "connectrpc.com/cors"
+	"github.com/kavkaco/Kavka-Core/config"
 	grpc_handlers "github.com/kavkaco/Kavka-Core/delivery/grpc/handlers"
 	"github.com/kavkaco/Kavka-Core/delivery/grpc/interceptor"
 	"github.com/kavkaco/Kavka-Core/infra/stream"
@@ -17,7 +20,19 @@ import (
 	"github.com/kavkaco/Kavka-ProtoBuf/gen/go/protobuf/events/v1/eventsv1connect"
 	"github.com/kavkaco/Kavka-ProtoBuf/gen/go/protobuf/message/v1/messagev1connect"
 	"github.com/kavkaco/Kavka-ProtoBuf/gen/go/protobuf/search/v1/searchv1connect"
+	"github.com/rs/cors"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
+
+func handleCORS(allowedOrigins []string, h http.Handler) http.Handler {
+	return cors.New(cors.Options{
+		AllowedOrigins:      allowedOrigins,
+		AllowedMethods:      []string{"POST"},
+		AllowedHeaders:      append(connectcors.AllowedHeaders(), []string{"X-Access-Token"}...),
+		AllowPrivateNetwork: true,
+	}).Handler(h)
+}
 
 type Services struct {
 	AuthService      *auth.AuthService
@@ -27,7 +42,7 @@ type Services struct {
 	StreamSubscriber stream.StreamSubscriber
 }
 
-func NewGrpcServer(router *http.ServeMux, services *Services) {
+func NewGrpcServer(cfg *config.HTTP, router *http.ServeMux, services *Services) error {
 	authInterceptor := interceptor.NewAuthInterceptor(services.AuthService)
 	interceptors := connect.WithInterceptors(authInterceptor)
 
@@ -46,9 +61,27 @@ func NewGrpcServer(router *http.ServeMux, services *Services) {
 	searchGrpcHandler := grpc_handlers.NewSearchGrpcHandler(log.NewSubLogger("message-handler"), services.SearchService)
 	searchGrpcRoute, searchGrpcRouter := searchv1connect.NewSearchServiceHandler(searchGrpcHandler, interceptors)
 
+	fmt.Println(authGrpcRoute)
 	router.Handle(authGrpcRoute, authGrpcRouter)
 	router.Handle(chatGrpcRoute, chatGrpcRouter)
 	router.Handle(eventsGrpcRoute, eventsGrpcRouter)
 	router.Handle(messageGrpcRoute, messageGrpcRouter)
 	router.Handle(searchGrpcRoute, searchGrpcRouter)
+
+	addr := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
+	handler := handleCORS(cfg.Cors.AllowOrigins, router)
+	server := &http.Server{
+		Addr:         addr,
+		Handler:      h2c.NewHandler(handler, &http2.Server{}),
+		ReadTimeout:  0,
+		WriteTimeout: 0,
+		IdleTimeout:  0,
+	}
+	fmt.Println("Grpc server is listening at ", addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
