de38a837243bf507203afea54d61ff7beab8ed58
RezaTG15 <77942073+rezatg@users.noreply.github.com>
2024-10-02 19:21:35 +0330

    Update user_vali.go

--- internal/service/user/user_vali.go
+++ internal/service/user/user_vali.go
@@ -3,5 +3,5 @@ package user
 type updateProfileValidation struct {
 	Name     string `validate:"required,min=3,max=40"`
 	LastName string `validate:"required,min=3,max=40"`
-	Username string `validate:"required,min=4,max=20"`
+	Username string `validate:"min=4,max=20"`
 }
